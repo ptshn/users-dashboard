a84a00f3e970c63a3f5df6bf952e097622b3207e
paul-lensabl <paul@lensabl.com>
2025-08-07 14:46:44 -0700

    Main - add react router dom library, set up routing for home and users

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
+import { Users } from './components/Users'
+import Home from './components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-
-
   return (
     <>
-      <div>
-        User dashboard
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="users" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
     </>
+
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <nav>
+            <Link to="/users">Users</Link>
+        </nav>
+    )
+}
+
+export default Home;
